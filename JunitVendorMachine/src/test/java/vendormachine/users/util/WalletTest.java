976151345ec20fa0815861db0a2f72d0e71ef683
Albert Kuc <albert.kuc@gmail.com>
2021-10-27 21:56:56 +0100

    refactor: add setup method to WalletTest
    setup() method initialises Wallet object prior to every test

--- JunitVendorMachine/src/test/java/vendormachine/users/util/WalletTest.java
+++ JunitVendorMachine/src/test/java/vendormachine/users/util/WalletTest.java
@@ -1,28 +1,37 @@
 package vendormachine.users.util;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WalletTest {
 
+    Wallet object;
+
+    @BeforeEach
+    public void setup() {
+        object = new Wallet("brandName", 200f);
+    }
+
     @Test
     public void constructor_TEST() {
-        Wallet object = new Wallet();
+        // Override object to test another constructor
+        object = new Wallet();
 
         assertTrue(object instanceof Wallet);
     }
 
     @Test
     public void constructor_wCredit_TEST() {
-        Wallet object = new Wallet(100f);
+        // Override object to test another constructor
+        object = new Wallet(100f);
 
         assertTrue(object instanceof Wallet);
     }
 
     @Test
     public void constructor_wCreditAndBrand_TEST() {
-        Wallet object = new Wallet("brandName", 100f);
 
         assertTrue(object instanceof Wallet);
 
@@ -30,21 +39,18 @@ class WalletTest {
 
     @Test
     public void getAllCredit_TEST() {
-        Wallet object = new Wallet();
 
-        assertEquals(0.5f, object.getAllCredit());
+        assertEquals(200, object.getAllCredit());
     }
 
     @Test
     public void getBrand_TEST() {
-        Wallet object = new Wallet("brandName", 200f);
 
         assertEquals("brandName", object.getBrand());
     }
 
     @Test
     public void addCredit_TEST() {
-        Wallet object = new Wallet(200f);
         object.addCredit(100f);
 
         assertEquals(300, object.getAllCredit());
@@ -52,32 +58,28 @@ class WalletTest {
 
     @Test
     public void getCredit_TEST() {
-        Wallet object = new Wallet(500f);
 
-        float credit = object.getCredit(100f);
-        assertEquals(100f, credit);
-        assertEquals(400f, object.getAllCredit());
-    }
-
-    @Test
-    public void getCreditFail_TEST() {
-        Wallet object = new Wallet(50f);
-
-        float credit = object.getCredit(100f);
-        assertEquals(0f, credit);
+        float credit = object.getCredit(150f);
+        assertEquals(150f, credit);
         assertEquals(50f, object.getAllCredit());
     }
 
     @Test
+    public void getCreditFail_TEST() {
+
+        float credit = object.getCredit(1000f);
+        assertEquals(0, credit);
+        assertEquals(200, object.getAllCredit());
+    }
+
+    @Test
     public void setCredit_TEST() {
-        Wallet object = new Wallet(150f);
         object.setCredit(1000f);
         assertEquals(1000, object.getAllCredit());
     }
 
     @Test
     public void setBrand_TEST() {
-        Wallet object = new Wallet();
         object.setBrand("newBrand");
         assertEquals("newBrand", object.getBrand());
     }
